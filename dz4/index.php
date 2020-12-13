eb906ac7b2111763787ebd7e6d356d3fbcc7ba5f
Dsergi <dsergo87@gmail.com>
2020-12-13 21:25:35 +0300

    DZ_4

---
+++ dz4/index.php
@@ -0,0 +1,35 @@
+<?php
+
+require 'vendor/autoload.php';
+require 'DB.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$twig = new \Twig\Environment($loader);
+
+$loadSize = 3;
+
+if (isset($_GET['from'])) {
+    $content = DB::getInstance()->getTableDataPart(DB::TABLE_GOODS, $_GET['from'] , $loadSize);
+    
+     
+    echo $twig->render('content.twig', [
+        'item' => $content,        
+    ]);
+    exit;
+}
+
+$action = $_GET['action'];
+$id = $_GET['id'];
+
+if($action == "open"){    
+    $content = DB::getInstance()->getById(DB::TABLE_GOODS, $id);
+    echo $twig->render('view.twig',[
+        'item'=>$content,
+    ]);
+    exit;
+}
+
+echo $twig->render('index.twig', [
+    'loadSize' => $loadSize,
+    
+]);
